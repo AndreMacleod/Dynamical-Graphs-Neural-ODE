e8eb8378e21a2a17e57979b794a57dba82dfde94
Andre Macleod <44291508+AndreMacleod@users.noreply.github.com>
2022-01-28 14:42:10 +1300

    Update project.jl

--- project.jl
+++ project.jl
@@ -1,4 +1,10 @@
-# packages
+using Pkg
+Pkg.activate(".")
+
+
+Pkg.instantiate()
+
+# Attaching packages
 using CSV
 using DataFrames
 using LightGraphs
@@ -8,7 +14,7 @@ using DataFrames
 using Random
 
 
-cd("C:/Users/andre_viking96/Desktop/New folder (2)/")
+#cd("C:/Users/andre_viking96/Desktop/New folder (2)/")
 
 # Loading df
 df = CSV.read("aves-wildbird-network.csv", DataFrame)
@@ -16,24 +22,32 @@ df = CSV.read("aves-wildbird-network.csv", DataFrame)
 # Improve df column names
 rename!(df,[:Bird1,:Bird2,:Weight, :Day])
 
-df
-describe(df)
+mini_df = filter([:Bird1, :Bird2] => (x, y) -> x < 6 && y < 6, df)
+
+mini_df
+describe(mini_df)
+
+
+
+gdf = groupby(mini_df, [:Day])
+combine(gdf, nrow)
+
 
 # Create graph from dataframe
-edge_df = df[:, [:Bird1, :Bird2, :Day]]
+edge_df = mini_df[:, [:Bird1, :Bird2, :Day]]
 edge_df
 
 full_mg = MetaGraph(edge_df, :Bird1, :Bird2, edge_attributes = [:Day])
+
 full_mg.graph
 typeof(full_mg)
 
 # save this graph object for leading and drawing in different file
-savegraph("birdgraph", full_mg.graph)
-
+savegraph("minibirdgraph", full_mg.graph)
 
 ## We now need a time series of graphs, 6 - one per each day
 
-# make a copy of ful graph that we can remove all edges from - this is the base with the entire vertex set
+# Make a copy of the full graph that we can remove all edges from - this is the base with the entire vertex set
 empty_graph = copy(full_mg)
 # remove all edges from this graph
 for edge in collect(edges(empty_graph))
@@ -41,7 +55,8 @@ for edge in collect(edges(empty_graph))
 end
 empty_graph
 
-# now, create separate dataframe for each time t, from whoich we will use th entries to fill the edges
+# Now, create separate dataframe for each time t, from whoich we will use the entries to fill the edges
+
 df_list = DataFrame[]
 for i in 1:6
     day_df = filter(:Day => ==(i), edge_df)
@@ -62,20 +77,30 @@ end
 
 time_graphs
 
+props(empty_graph, 25)
 
-# Testing - looks good
-t5 = time_graphs[5]
-collect(edges(t5))[150:180]
-filter(:Bird2 => ==(4), df_list[5])
+## Testing to make sure vetrex labels match index for when we use add_edge()
 
-# use adjacency matrix function
-A1 = adjacency_matrix(time_graphs[1])
+a = 0
+for i in 1:10
+    if get_prop(empty_graph, i, :name) != i
+        print("incorrect name")
+        break
+    else
+        a += 1
+    end        
+end
+print(a)
 
+time_graphs[5]
 
 ## Doing the embedding
 
 using LinearAlgebra
 using Arpack
+
+n = 5
+d = 2
 
 # function that does svd decomposition
 function do_the_rdpg(A,d)
@@ -85,22 +110,20 @@ function do_the_rdpg(A,d)
     return (L̂ = L̂, R̂ = R̂)
 end
 
-# check it is diagonal matrix
-@assert A1 == A1'
 
 time_embeddings = Matrix{Float64}[]
 
 # Create a list of embeddings that will contain our time series of graph embeddings
 for graph in time_graphs
-    L, R = do_the_rdpg(adjacency_matrix(graph), 3)
+    L, R = do_the_rdpg(adjacency_matrix(graph), d)
     push!(time_embeddings, L)
 end
 
-time_embeddings[1]
+time_embeddings[5]
 length(time_embeddings)
 
 # Having a look at our L and R matrices
-L1, R1 = do_the_rdpg(A1, 4)
+#L1, R1 = do_the_rdpg(A1, 4)
 
 
 ## Solving neural ODE
@@ -111,8 +134,7 @@ using DiffEqFlux
 using OrdinaryDiffEq
 using Flux
 using Optim
-using ProgressMeter
-using ProgressLogging
+
 
 
 # Create training set
@@ -129,7 +151,7 @@ tsteps = range(tspan[1], tspan[2], length = datasize)
 # In this case, the imput will be the distance from one vertex to each other vertex, for every vertex in the graph, repeated for every vertex
 
 # Function that takes take vertex embedding, one vertex and returns all distances between that vertex and all other vertices
-function vertex_distances(M::Matrix{Float64}, v::Int64)
+function vertex_distances(M, v)
     distances = []
     for i in 1:size(M)[1]-1              # iterating over all rows in matrix except the one selected
         for j in 1:size(M)[2]            # iterating over all columns in matrix
@@ -141,16 +163,14 @@ end
 
 vertex_distances(u0, 2)
 
-# Defining dimension of matrix - n is n. of vertices, d is the dimension of each vertex
-n = 202
-d = 3
 
 # create model architecture
-ann = Chain(Dense((n-1)*d,640,tanh), Dense(640,3))
+ann = Chain(Dense((n-1)*d,8,tanh), Dense(8,d))
 pinit,re = Flux.destructure(ann)
 
 # check how many params in the nn - 388483
 pinit
+
 
 # create initial function
 function dudt1_(du,u,p,t)
@@ -158,34 +178,32 @@ function dudt1_(du,u,p,t)
         for j in 1:d
             du[(i-1)*d + j]= re(p)(vertex_distances(u, i))[j]
         end
-    @info "foo-bar" progress=i/n  # this will show us that we are iterating through the matrix
+    #@info "foo-bar" progress=i/n  # this will show us that we are iterating through the matrix
     end
 end
 
 # Define problem
+
 prob = ODEProblem(dudt1_, u0, tspan, pinit)
 
 # Solving problem
-
 # Reshaping train so it can be fitted aginst loss function
 loss_train = Array{Float64}(undef,n,d,datasize)
 train[1]
 for i in 1:datasize
      loss_train[:,:,i] = train[i]
 end
+loss_train
 
 # flatening loss_train to conform with shape during nn training
 loss_train = reshape(loss_train, (n*d,datasize))
 
+plot(loss_train', linestyle = :dot, linealpha = 0.8, linewidth = 1.5)
 # Create loss function
 function loss(p)
     tmp_prob = remake(prob, p = p)
-    tmp_sol = solve(tmp_prob, Tsit5(), saveat = tsteps, progress = true)
+    tmp_sol = solve(tmp_prob, Tsit5(), saveat = tsteps)
     sol_array = reshape(Array(tmp_sol), (n*d,datasize))
-    println("TRAINING")
-    println(loss_train)
-    println("NNVALS")
-    println(sol_array)
     sum(abs2, sol_array - loss_train)
 
 end
@@ -196,20 +214,79 @@ loss(pinit)
 # Neural callback function used for nn training
 function neuralode_callback(p,l)
     @show l
-    @show p
     tmp_prob = remake(prob, p = p)
     tmp_sol = solve(tmp_prob, Tsit5(), saveat = tsteps)
-    fig = plot(tmp_sol)
-    display(fig)
+    plot(tmp_sol)
+    data_plot = plot!(loss_train', linestyle = :dot, linealpha = 0.8, linewidth = 1.5)
+    display(data_plot)
     false
 end
 
 
 #Use ADAM for finding initial local minima
-@time res = DiffEqFlux.sciml_train(loss, pinit, ADAM(0.05), cb = neuralode_callback, maxiters = 5)
+res = DiffEqFlux.sciml_train(loss, pinit, ADAM(0.05), cb = neuralode_callback, maxiters = 100)
 
 # res.minimzers gives best parameters from first search, now use these to continue with BFGS which finishes local minima better
 # bgfs runs until finds a fit - finalizes optimization - bfgs good for stiff conditions
 # bfgs looks for convergences criteria
-res2 = DiffEqFlux.sciml_train(loss, res.minimizer, BFGS(initial_stepnorm=0.01), cb = neuralode_callback, maxiters = 5)
+res2 = DiffEqFlux.sciml_train(loss, res.minimizer, BFGS(initial_stepnorm=0.02), cb = neuralode_callback, maxiters = 50)
+
+res2.minimum
+
+# Generate data given by neural network with these params
+pred_prob = ODEProblem(dudt1_, u0, tspan, res2.minimizer)
+pred_data = solve(pred_prob, Tsit5(), saveat = tsteps)
+
+
+using ModelingToolkit
+using DataDrivenDiffEq
+using OrdinaryDiffEq
+using Sundials
+
+X = (reshape(Array(pred_data),  (n*d,datasize)))
+DX = (reshape(Array(pred_data(pred_data.t, Val{1})), (n*d,datasize)))
+
+DX
+X
+
+
+# create basis
+@variables t, u[1:10](t)
+# Generate the basis functions, multivariate polynomials up to deg 5
+# and sine
+b = [polynomial_basis(u, 4); sin.(u)]
+basis = Basis(b, u, iv = t)
+# Create the thresholds which should be used in the search process
+λ = Float32.(exp10.(-5:0.1:0))
+# Create an optimizer for the SINDy problem
+opt = STLSQ(λ)
+
+# Define problem with data and differentiated data 
+ddprob = ContinuousDataDrivenProblem(X, DX)
+
+# find solution within basis
+ddsol = solve(ddprob, basis, opt, normalize = true, maxiter = 10000)
+
+# System of equations
+system = result(ddsol)
+print(system)
+
+# Current parameters for the equations
+params = parameters(ddsol);
+
+print(ddsol, Val{false})
+
+# create ODE function
+equations(system)
+@named sys = ODESystem(equations(system), independent_variable(system), states(system), parameters(system))
+
+dudt = ODEFunction(sys)
+
+# prediction over 6 time steps
+pred_tspan = (1,6)
+pred_tsteps = range(pred_tspan[1], pred_tspan[2], length = datasize+1)
+
+# Solve ODE to get data
+prob = ODEProblem(dudt, u0, pred_tspan, params)
+pred_sol = solve(prob,  Vern7(), saveat = pred_tsteps, maxiters = 1000000)
 
